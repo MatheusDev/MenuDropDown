49e407d97e237b0f440e91599e5f748d306c3096
Matheus <53701869+MatheusDev@users.noreply.github.com>
2020-05-27 10:53:57 -0300

    hghg

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,18 +5,25 @@ class Header extends Component {
   constructor(props){
 		super(props)
 		this.state = {
-			cursos: false
+      cursosKey: false,
+      subMenu:  <ul className="sub-menu">
+                  <li><a href="#">JavaScript</a></li><br/>
+                  <li><a href="#">React.js</a></li><br/>
+                  <li><a href="#">Node.js</a></li><br/>
+                  <li><a href="#">MySQL</a></li>
+                </ul>
 		}
-	}
+  }
+  mouseOut = (className = 'sub-menu') => {
+    if(this.state.subMenu){
+      this.setState({cursosKey:true})
+    }
+    this.setState({cursosKey:false})
+  }
 	cursos = () => {
-		if(this.state.cursos){
+		if(this.state.cursosKey){
 			return (
-				<ul className="cursos">
-					<li><a href="#">JavaScript</a></li>
-					<li><a href="#">React.js</a></li>
-					<li><a href="#">Node.js</a></li>
-					<li><a href="#">MySQL</a></li>
-				</ul>
+				this.state.subMenu
 			)
 		}
 	}
@@ -29,14 +36,8 @@ class Header extends Component {
               <li ><a className="main-menu" href="#">Home</a></li>
               <li ><a className="main-menu" href="#">Reportar DMCA</a></li>
               <li ><a className="main-menu" href="#">Contato</a></li>
-              <li ><a className="main-menu" href="#"onClick={() => this.setState({cursos:true})} onMouseOut={() => this.setState({cursos:false})}>Cursos</a>
-								{/*this.cursos()*/}
-                <ul>
-					        <li><a href="#">JavaScript</a></li><br/>
-					        <li><a href="#">React.js</a></li><br/>
-					        <li><a href="#">Node.js</a></li><br/>
-					        <li><a href="#">MySQL</a></li>
-				        </ul>
+              <li ><a className="main-menu" href="#"onMouseEnter={() => this.setState({cursosKey:true})} onMouseOut={() => this.mouseOut()}>Cursos</a>
+								{this.cursos()}
 							</li>
 							<li ><a className="main-menu" >Ajuda</a></li>
 						</ul>
