105db215b2cb94805fd3328ebd29dad91034f8b2
Matheus <53701869+MatheusDev@users.noreply.github.com>
2020-05-30 10:55:24 -0300

    update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -15,7 +15,7 @@ class Header extends Component {
   }
   showSubMenu = () => {
     this.setState({subMenu:
-      <ul className="sub-menu" onMouseEnter={} style={{display: "grid"}}>
+    <ul className="sub-menu" onMouseEnter={()=>this.showSubMenu()} style={{display: "grid"}}>
       <li><a href="#">JavaScript</a></li>
       <li><a href="#">React.js</a></li>
       <li><a href="#">Node.js</a></li>
@@ -23,7 +23,7 @@ class Header extends Component {
     </ul>
     })
   }
-  
+
 	render(){
     const subMenu = this.state.subMenu
 		return(
