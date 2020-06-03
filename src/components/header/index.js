6e8fd4cc0696ab3cd98c664a6e8b946291c0a8a7
Matheus <53701869+MatheusDev@users.noreply.github.com>
2020-06-02 22:41:32 -0300

    update header style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,7 +8,6 @@ class Header extends Component {
        
 		}
   }
-  
 	render(){
     return(
       <header>
@@ -23,7 +22,14 @@ class Header extends Component {
         <nav className="menu">
           <ul>
             <li><a href="#inicio">Início</a></li>
-            <li><a href="#cursos">Cursos</a></li>
+            <li><a href="#cursos">Cursos</a>
+              <ul className="">
+                <li><a href="#">JavaScript</a></li>
+                <li><a href="#">ReactJS</a></li>
+                <li><a href="#">Node</a></li>
+                <li><a href="#">ReactNative</a></li>
+              </ul>
+            </li>
             <li><a href="#sobre">Sobre</a></li>
             <li><a href="#contato">Contato</a></li>
           </ul>
