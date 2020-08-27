4f67da1d10780eadc85e9d43e7a3316018a927de
Matheus <53701869+MatheusDev@users.noreply.github.com>
2020-08-27 11:40:26 -0300

    update style and header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -15,19 +15,25 @@ class Header extends Component {
         </div>
         <button className="menu-toggle">
         &#9776;
-         {/*<i className="fa fa-lg fa-bars"></i>*/}
         </button>
         <nav className="menu">
           <ul>
             <li><a href="#inicio">Início</a></li>
-            <li><a href="#cursos">Cursos</a></li>
+            <li><a href="#cursos">Cursos</a>
+              <ul>
+                <li><a href="#cursos/javascript">JavaScript</a></li>
+                <li><a href="#cursos/python">Python</a></li>
+                <li><a href="#cursos/reactjs">ReactJS</a></li>
+                <li><a href="#cursos/nodejs">NodeJS</a></li>
+              </ul>
+            </li>
             <li><a href="#sobre">Sobre</a></li>
             <li><a href="#contato">Contato</a></li>
           </ul>
         </nav>
         <aside className="autenticacao">
           <a href="#login">Login</a>
-          <a href="#registrar" className="botao destaque">Registrar</a>
+          <a href="#registrar" className="destaque">Registrar</a>
         </aside>
       </header>
     )
